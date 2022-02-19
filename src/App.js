af178b9cd52055ec392d1cb9c50edc2bd63fe856
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-02-19 23:28:37 +0100

    refactor routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import User from './pages/User.js';
 import SignUp from './pages/SignUp.js';
 import Login from './pages/Login.js';
 import Users from './pages/Users.js';
+import NotAuth from './pages/NotAuth.js';
+import Followed from './pages/Follow.js';
+import Navigation from './components/Navigation/Navigation.js';
 
 function App() {
 	const dispatch = useDispatch();
@@ -18,11 +21,14 @@ function App() {
 
 	return (
 		<>
+			<Navigation />
 			<Routes>
 				<Route path='login' element={<Login />} />
 				<Route path='signup' element={<SignUp />} />
-				<Route path='user' element={<User />} />
 				<Route path='users' element={<Users />} />
+				<Route path='users/:userId' element={<User />} />
+				<Route path='users/:userId/follow' element={<Followed />} />
+				<Route path='/not-authorized' element={<NotAuth />} />
 				<Route path='/' element={<Navigate to='/login' />} />
 				<Route path='*' element={<div>ERROR 404 - not found </div>} />
 			</Routes>
